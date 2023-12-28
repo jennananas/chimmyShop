94a03881eeb95c074016f820079b98c05c7bcc6e
jennananas <100166354+jennananas@users.noreply.github.com>
2023-12-28 23:49:02 +0100

    responsive signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -22,47 +22,82 @@ export default function SignIn() {
     };
 
     return (
-        <div className="w-100 h-screen flex flex-col">
+        <div className="max-w-screen h-screen flex flex-col">
             <Header />
             <div className="flex grow">
-                <div
-                    className={`flex-1 bg-zinc-100 transition-transform duration-500 ease-in-out ${isSignUp ? 'transform translate-x-full' : 'transform translate-x-0'
-                        }`}
-                >
+                <div className="relative md:hidden">
                     <div className="flex items-center justify-center h-full">
-                        <img src="src/assets/images/coverSI.jpg" alt="Palm Trees" className="h-full w-full" />
+                        <img src="src/assets/images/coverSI.jpg" alt="Palm Trees" className="h-full w-full object-cover" />
                     </div>
-                </div>
-                <div className="flex-1 flex items-center justify-center">
-                    <div
-                        className={`flex-1 flex-col items-center justify-center transition-transform duration-500 ease-in-out ${isSignUp ? 'transform -translate-x-full' : 'transform translate-x-0'
-                            }`}
-                    >
+                    <div className="absolute top-[15%] w-full h-2/3 bg-white/30 backdrop-blur-sm
+                    flex flex-col justify-center">
                         <div className="mb-4 text-center">
                             <button
-                                className="text-blue-500 underline"
+                                className="text-blue-900 underline"
                                 onClick={toggleForm}
                             >
                                 {isSignUp ? 'Already have an account ? Sign In' : 'No account yet ? Sign Up'}
                             </button>
                         </div>
                         {isSignUp ? (
-                            <form className="p-10 flex flex-col items-center shadow-lg shadow-zinc-100" onSubmit={(e) => e.preventDefault}>
+                            <form className="p-10 flex flex-col items-center" onSubmit={(e) => e.preventDefault}>
                                 <Form fields={signUpFields}></Form>
-                                <button type="submit" className=" bg-green-600 text-white p-2 mt-10 w-24 rounded">
+                                <button type="submit" className=" bg-green-700 text-white p-2 mt-10 w-24 rounded">
                                     Sign Up
                                 </button>
                             </form>
                         ) : (
-                            <form className="p-10 flex flex-col items-center shadow-lg shadow-zinc-100" onSubmit={(e) => e.preventDefault}>
+                            <form className="p-10 flex flex-col items-center" onSubmit={(e) => e.preventDefault}>
                                 <Form fields={signInFields}></Form>
-                                <button type="submit" className=" bg-green-600 text-white p-2 mt-10 w-24 rounded">
+                                <button type="submit" className=" bg-green-700 text-white p-2 mt-10 w-24 rounded">
                                     Sign In
                                 </button>
                             </form>
                         )}
+
                     </div>
                 </div>
+                <div className="hidden md:flex">
+                    <div
+                        className={`flex-1 md:bg-zinc-100 md:transition-transform md:duration-500 md:ease-in-out ${isSignUp ? 'md:transform md:translate-x-full' : 'md:transform md:translate-x-0'
+                            }`}
+                    >
+                        <div className="flex items-center justify-center h-full">
+                            <img src="src/assets/images/coverSI.jpg" alt="Palm Trees" className="h-full w-full object-cover" />
+                        </div>
+                    </div>
+                    <div className="flex-1 flex items-center justify-center">
+                        <div
+                            className={`flex-1 flex-col items-center justify-center md:transition-transform md:duration-500 md:ease-in-out ${isSignUp ? 'md:transform md:-translate-x-full' : 'md:transform md:translate-x-0'
+                                }`}
+                        >
+                            <div className="mb-4 text-center">
+                                <button
+                                    className="text-blue-900 underline"
+                                    onClick={toggleForm}
+                                >
+                                    {isSignUp ? 'Already have an account ? Sign In' : 'No account yet ? Sign Up'}
+                                </button>
+                            </div>
+                            {isSignUp ? (
+                                <form className="p-10 flex flex-col items-center shadow-lg shadow-zinc-100" onSubmit={(e) => e.preventDefault}>
+                                    <Form fields={signUpFields}></Form>
+                                    <button type="submit" className=" bg-green-700 text-white p-2 mt-10 w-24 rounded">
+                                        Sign Up
+                                    </button>
+                                </form>
+                            ) : (
+                                <form className="p-10 flex flex-col items-center shadow-lg shadow-zinc-100" onSubmit={(e) => e.preventDefault}>
+                                    <Form fields={signInFields}></Form>
+                                    <button type="submit" className=" bg-green-700 text-white p-2 mt-10 w-24 rounded">
+                                        Sign In
+                                    </button>
+                                </form>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <Footer />
         </div>
